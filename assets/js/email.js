09f3c2ed6c1d96064e0cd2e58e462ef7d2c67015
armerpunkt <armerpunkt@users.noreply.github.com>
2021-05-23 20:24:28 -0700

    Update email.js

--- assets/js/email.js
+++ assets/js/email.js
@@ -1,11 +1,5 @@
-function defer(method) {
-    if (window.jQuery) {
-        method();
-    } else {
-        setTimeout(function() { defer(method) }, 50);
-    }
-}
-
-defer(function () {
-  $('#email').html("\u003Cn uers=\"znvygb:fpvraprfhcresevraqf@tznvy.pbz\" ery=\"absbyybj\"\u003E Rznvy\u003C/n\u003E".replace(/[a-zA-Z]/g,function(c){return String.fromCharCode((c<="Z"?90:122)>=(c=c.charCodeAt(0)+13)?c:c-26);}));
+window.addEventListener('load', function () {
+    console.log('email');
+    element = document.getElementById('email');
+    element.innerHTML = "\u003Cn uers=\"znvygb:fpvraprfhcresevraqf@tznvy.pbz\" ery=\"absbyybj\"\u003E Rznvy\u003C/n\u003E".replace(/[a-zA-Z]/g,function(c){return String.fromCharCode((c<="Z"?90:122)>=(c=c.charCodeAt(0)+13)?c:c-26);});
 });
