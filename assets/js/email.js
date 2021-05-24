151fe29ea3a56595be6856f7b39bb5988114e1f2
armerpunkt <armerpunkt@users.noreply.github.com>
2021-05-23 20:29:49 -0700

    Update email.js

--- assets/js/email.js
+++ assets/js/email.js
@@ -1,5 +1,4 @@
 window.addEventListener('load', function () {
-    console.log('email');
     element = document.getElementById('email');
     element.innerHTML = "\u003Cn uers=\"znvygb:fpvraprfhcresevraqf@tznvy.pbz\" ery=\"absbyybj\"\u003E Rznvy\u003C/n\u003E".replace(/[a-zA-Z]/g,function(c){return String.fromCharCode((c<="Z"?90:122)>=(c=c.charCodeAt(0)+13)?c:c-26);});
 });
